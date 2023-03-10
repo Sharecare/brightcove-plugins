6dc9f946139e7fc8aa1929efe2dddbaa93a9704a
Liam Mitchell <liam.mitchell@sharecare.com>
2023-03-10 14:56:17 +0100

    Captions plugin

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # brightcove-plugins
 Scripts to allow integration with [Brightcove player](https://studio.brightcove.com/products/videocloud/players/players/l6USwnzxO) through [plugin API](https://player.support.brightcove.com/plugins/brightcove-player-plugins.html).
 
-Used in `you.sharecare.com` player (https://studio.brightcove.com/products/videocloud/players/players/l6USwnzxO) by referencing public GH path (https://raw.githubusercontent.com/Sharecare/brightcove-plugins/master/events.js).
+Plugin is referenced by public GH URL (https://raw.githubusercontent.com/Sharecare/brightcove-plugins/master/events.js).
 
-
+Used in `you.sharecare.com` player (https://studio.brightcove.com/products/videocloud/players/players/l6USwnzxO).
---
+++ captions.js
@@ -0,0 +1,25 @@
+// Based on https://github.com/BrightcoveLearning/18750-automatically-set-caption-language/blob/d27d15d4d6078bc8e3d08ac7d2b28ad6d8660c27/plugin/auto-language.js
+// Brightcove already has APIs to set language & caption lang using query params.
+// Neither works...
+videojs.registerPlugin('captions', function () {
+  var player = this
+
+  player.on("loadedmetadata", function () {
+    var language = player.language().toLowerCase()
+    var tracks = Array.from(player.textTracks());
+
+    // Prefer exact match, fall back to base lang.
+    var preferredTrack = tracks.find(function (track) {
+      track.language.toLowerCase() === language;
+    }) || tracks.find(function (track) {
+      track.language.toLowerCase().slice(0, 2) === language.slice(0, 2);
+    });
+
+    if (preferredTrack) {
+      tracks.forEach(function (track) {
+        track.mode = "disabled";
+      });
+      preferredTrack.mode = "showing";
+    }
+  });
+});
